4994847744587ae13e99c05d6f39aed3d00b54cc
SehwaGod <jrhong95@gmail.com>
2020-03-21 05:05:20 +0900

    homework finished!
    공부하자세화야

--- CSproject/Program.cs
+++ CSproject/Program.cs
@@ -8,78 +8,132 @@ namespace CSproject
             public int m_nNumber;
             public int m_nScore;
             public int m_nRank;
-             public Student(int nNumber, int nScore, int nRank = 0){
+            public string m_nGrade;
+            public Student(int nNumber, int nScore, int nRank = 0, string nGrade = null){
                  m_nNumber = nNumber;
-                 m_nScore = nScore;
-                 m_nRank = nRank;
-             }
-
-             public void getRank(int nRank){
-                 m_nRank = nRank;
+                 m_nScore  = nScore;
+                 m_nRank   = nRank;
+                 m_nGrade  = nGrade;
              }
         }
+
         static void Main(string[] args)
         {
             Array arrRecord = Array.CreateInstance(typeof(Student), 5);
-
+            int studentNumber = arrRecord.Length;
             Console.WriteLine("5명의 학생의 성적을 입력하세요.");
 
-            for(int ii = 0; ii < arrRecord.Length; ii++){
+            for (int i = 0; i < studentNumber; i++)
+            {
                 string strScore = Console.ReadLine();
-                arrRecord.SetValue(new Student(ii + 1, Convert.ToInt32(strScore)), ii);
+                arrRecord.SetValue(new Student(i + 1, Convert.ToInt32(strScore)), i);
             }
 
-            int curRank = 1;
-            int i = 0;
-            
-            while(i < arrRecord.Length){
-                int overlapCount = 0;
-                Console.WriteLine("i {0} arryRecord: {1}", i, arrRecord.Length); 
-                if(((Student)arrRecord.GetValue(i)).m_nRank == 0){
-                    int tmpScore = ((Student)arrRecord.GetValue(i)).m_nScore;
-                    
-                    for(int j = 0; j < arrRecord.Length; j++){
-                        if(tmpScore < ((Student)arrRecord.GetValue(j)).m_nScore && ((Student)arrRecord.GetValue(j)).m_nRank == 0)
-                            //랭크가 정해지지 않은 것들 중에서 가장 높은 점수 찾는다.
-                            tmpScore = ((Student)arrRecord.GetValue(j)).m_nScore;
-                            Console.WriteLine("Current Big Score: {0}", tmpScore);
-                    }
+            //순서대로정렬
+            for (int i = 0; i < studentNumber; ++i)
+            {
 
-                    for(int j = 0; j < arrRecord.Length; j++){
-                        if(tmpScore == ((Student)arrRecord.GetValue(j)).m_nScore && ((Student)arrRecord.GetValue(j)).m_nRank == 0){
-                            //((Student)arrRecord.GetValue(j)).getRank(curRank);
-                            ((Student)arrRecord.GetValue(j)).m_nRank = curRank;
-                            overlapCount++;
-                        }
-                    }
-                    curRank += overlapCount;
-                    for(int j = 0; j < arrRecord.Length; j++){
-                        //순위가 정해지지 않은 학생이 있는 경우 다시 처음부터 검색
-                        if(((Student)arrRecord.GetValue(j)).m_nRank != 0){
-                            i = 0;
-                            continue;
-                        }
+                //for (int j = 0; j < studentNumber; j++)
+                //{
+                //    Console.WriteLine("{0}등  {1}번 학생, {2}점, {3}", ((Student)arrRecord.GetValue(j)).m_nRank,
+                //        ((Student)arrRecord.GetValue(j)).m_nNumber,
+                //        ((Student)arrRecord.GetValue(j)).m_nScore,
+                //        ((Student)arrRecord.GetValue(j)).m_nGrade);
+                //}
+                //Console.WriteLine("{0}=======================", i);
+
+                int curScore = ((Student)arrRecord.GetValue(i)).m_nScore;
+
+                if (i < studentNumber - 1)
+                {
+                    int nextScore = ((Student)arrRecord.GetValue(i + 1)).m_nScore;
+                    if (curScore < nextScore)
+                    {
+                        int tNumber = ((Student)arrRecord.GetValue(i)).m_nNumber;
+                        int tScore = ((Student)arrRecord.GetValue(i)).m_nScore;
+                        int tRank = ((Student)arrRecord.GetValue(i)).m_nRank;
+                        string tGrade = ((Student)arrRecord.GetValue(i)).m_nGrade;
+
+                        //Console.WriteLine("TEMP!!!!!! {0}등  {1}번 학생, {2}점, {3}", tRank, tNumber, tScore, tGrade);
+ 
+                        ((Student)arrRecord.GetValue(i)).m_nNumber = ((Student)arrRecord.GetValue(i + 1)).m_nNumber;
+                        ((Student)arrRecord.GetValue(i)).m_nScore =  ((Student)arrRecord.GetValue(i + 1)).m_nScore;
+                        ((Student)arrRecord.GetValue(i)).m_nRank =   ((Student)arrRecord.GetValue(i + 1)).m_nRank;
+                        ((Student)arrRecord.GetValue(i)).m_nGrade =  ((Student)arrRecord.GetValue(i + 1)).m_nGrade;
+
+                        ((Student)arrRecord.GetValue(i + 1)).m_nNumber = tNumber;
+                        ((Student)arrRecord.GetValue(i + 1)).m_nScore = tScore;
+                        ((Student)arrRecord.GetValue(i + 1)).m_nRank = tRank;
+                        ((Student)arrRecord.GetValue(i + 1)).m_nGrade = tGrade;
+
+                        i = -1;
                     }
                 }
-                i++;
             }
-
-            for(int ii = 0; ii < arrRecord.Length; ii++){
-                int nNumber = ((Student)arrRecord.GetValue(ii)).m_nNumber;
-                int nScore = ((Student)arrRecord.GetValue(ii)).m_nScore;
-                int nRank = ((Student)arrRecord.GetValue(ii)).m_nRank;
+           
+            int overlapCount = 0;
+            int curRank = 1;
+            for (int i = 0; i < studentNumber; i++)
+            {
+                if (i == 0) {
+                    //Console.WriteLine("{0} Done.", i);
+                    ((Student)arrRecord.GetValue(i)).m_nRank = curRank;
+                    overlapCount++; 
+                }
+                else if (i != 0 && ((Student)arrRecord.GetValue(i - 1)).m_nScore == ((Student)arrRecord.GetValue(i)).m_nScore)
+                {
+                    //Console.WriteLine("{0} Done.", i);
+                    ((Student)arrRecord.GetValue(i)).m_nRank = curRank;
+                    overlapCount++;
+                }
+                else
+                {
+                    //Console.WriteLine("{0} Done.", i);
+                    curRank += overlapCount;
+                    overlapCount = 0;
+                    ((Student)arrRecord.GetValue(i)).m_nRank = curRank;
+                    overlapCount++;
+                }
+                    
+            }
+            //Console.WriteLine("dddddddddddddddddd");
+            //for (int j = 0; j < studentNumber; j++)
+            //{
+            //    Console.WriteLine("{0}등  {1}번 학생, {2}점, {3}", ((Student)arrRecord.GetValue(j)).m_nRank,
+            //        ((Student)arrRecord.GetValue(j)).m_nNumber,
+            //        ((Student)arrRecord.GetValue(j)).m_nScore,
+            //        ((Student)arrRecord.GetValue(j)).m_nGrade);
+            //}
+            //등급 저장
+            for (int i = 0; i < studentNumber; i++)
+            {
+                int nScore = ((Student)arrRecord.GetValue(i)).m_nScore;
                 string strGrade = "F";
 
-                if(nScore >= 90)
+                if (nScore >= 90)
                     strGrade = "A";
-                else if(nScore >= 80)
+                else if (nScore >= 80)
                     strGrade = "B";
-                else if(nScore >= 70)   
+                else if (nScore >= 70)
                     strGrade = "C";
-                else if(nScore >= 60)
+                else if (nScore >= 60)
                     strGrade = "D";
-                
-                Console.WriteLine("{0}번학생 성적: {1}   등급: {2}  등 수: {3}", nNumber, nScore, strGrade, nRank);
+
+                ((Student)arrRecord.GetValue(i)).m_nGrade = strGrade;
+            }
+
+            for(int n = 0; n < studentNumber; n++){
+
+                for(int i = 0; i < studentNumber; i++) {
+                    int nRank = ((Student)arrRecord.GetValue(i)).m_nRank;
+                    int nNumber = ((Student)arrRecord.GetValue(i)).m_nNumber;
+                    int nScore = ((Student)arrRecord.GetValue(i)).m_nScore;
+                    string nGrade = ((Student)arrRecord.GetValue(i)).m_nGrade;
+
+                    if (nRank == n + 1){
+                        Console.WriteLine("{0}등  {1}번 학생, {2}점, {3}", nRank, nNumber, nScore, nGrade);
+                    }
+                }
             }
         }
     }
