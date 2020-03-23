517ef5ffb4ed868833775a7e52391545ebefcfba
jrhong95 <jrhong95@gmail.com>
2020-03-23 18:41:48 +0900

    3/23 hw changed

---
+++ os_project/hw1/hw1.c
@@ -0,0 +1,24 @@
+#include "hw1.h"
+
+void Init(){
+    
+}
+
+void InsertObjectToTail(Object* pObj, int ObjNum){
+
+}
+
+void InsertObjectToHead(Object* pObj, int objNum){
+
+}
+
+Object* GetObjectByNum(int objnum){    
+}
+
+BOOL DeleteObject(Object* pObj){
+}
+
+void InsertObjectIntoObjFreeList(Object* pObj){
+
+}
+
---
+++ os_project/hw1/main.c
@@ -0,0 +1,8 @@
+#include "hw1.h"
+
+int main(){
+    printf("Test\n");
+    return 0;
+}
+
+
