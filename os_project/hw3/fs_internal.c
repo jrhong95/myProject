4c7faa400f21989dad777fe38edec9da87add023
jrhong95 <jrhong95@gmail.com>
2020-05-27 15:13:29 +0900

    hw3_1 done

--- os_project/hw3/fs_internal.c
+++ os_project/hw3/fs_internal.c
@@ -1,51 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "disk.h"
 #include "fs.h"
 
+void FileSysInit(){
+    int i;
+    char* buf;
+    // Init Superblock, bytemap
+    for(i = 0; i < INODELIST_BLOCK_FIRST; i++){
+        buf = (char*)calloc(BLOCK_SIZE, 1);
+        DevWriteBlock(i, buf);
+    }
+    // Init Inode list
+    for(i = INODELIST_BLOCK_FIRST; i < 7; i++){
+        buf = (char*)calloc(BLOCK_SIZE/sizeof(int), sizeof(int));
+        DevWriteBlock(i, buf);
+    } 
+}
+
 void SetInodeBytemap(int inodeno)
 {
+    char* buf = (char*)malloc(BLOCK_SIZE);
 
+    DevReadBlock(INODE_BYTEMAP_BLOCK_NUM, buf);
+    buf[inodeno] = 1;
+    DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM, buf);
 }
 
 
 void ResetInodeBytemap(int inodeno)
 {
+    char* buf = (char*)malloc(BLOCK_SIZE);
 
+    DevReadBlock(INODE_BYTEMAP_BLOCK_NUM, buf);
+    buf[inodeno] = 0;
+    DevWriteBlock(INODE_BYTEMAP_BLOCK_NUM, buf);
 }
 
 
 void SetBlockBytemap(int blkno)
 {
+    char* buf = (char*)malloc(BLOCK_SIZE);
 
+    DevReadBlock(BLOCK_BYTEMAP_BLOCK_NUM, buf);
+    buf[blkno] = 1;
+    DevWriteBlock(BLOCK_BYTEMAP_BLOCK_NUM, buf);
 }
 
 
 void ResetBlockBytemap(int blkno)
 {
+    char* buf = (char*)malloc(BLOCK_SIZE);
 
+    DevReadBlock(BLOCK_BYTEMAP_BLOCK_NUM, buf);
+    buf[blkno] = 0;
+    DevWriteBlock(BLOCK_BYTEMAP_BLOCK_NUM, buf);    
 }
 
 
 void PutInode(int inodeno, Inode* pInode)
-{
+{   //0~15: 3, 16~31: 4, 32~47: 5, 48~63: 6
+    int i;
+    int blkno = inodeno / NUM_OF_INODE_PER_BLOCK + INODELIST_BLOCK_FIRST;
+    int inodeByteStart = inodeno * sizeof(Inode);
+    char* buf = (char*)malloc(BLOCK_SIZE);
 
+    DevReadBlock(blkno, buf);
+
+    // Copy pInode to buffer
+    buf[inodeByteStart] = pInode->allocBlocks;
+    buf[inodeByteStart + 1] = pInode->size;
+    buf[inodeByteStart + 2] = pInode->type;
+    for(i = 0; i < NUM_OF_DIRECT_BLOCK_PTR; i++){
+        buf[inodeByteStart + 3 + i] = pInode->dirBlockPtr[i];
+    }
+
+    DevWriteBlock(blkno, buf);
 }
 
-
 void GetInode(int inodeno, Inode* pInode)
-{
+{    //0~15: 3, 16~31: 4, 32~47: 5, 48~63: 6
+    int i;
+    int blkno = inodeno / NUM_OF_INODE_PER_BLOCK + INODELIST_BLOCK_FIRST;
+    int inodeByteStart = inodeno * sizeof(Inode);
+    char* buf = (char*)malloc(BLOCK_SIZE);
 
+    DevReadBlock(blkno, buf);
+
+    // Copy buffer to pInode
+    pInode->allocBlocks = (int)buf[inodeByteStart];
+    pInode->size = (int)buf[inodeByteStart + 1];
+    pInode->type = (int)buf[inodeByteStart + 2];
+    for(i = 0; i < NUM_OF_DIRECT_BLOCK_PTR; i++){
+        pInode->dirBlockPtr[i] = (int)buf[inodeByteStart + 3 + i];
+    }
 }
 
 
 int GetFreeInodeNum(void)
 {
+    int i;
+    char* buf = (char*)malloc(BLOCK_SIZE);
 
+    DevReadBlock(INODE_BYTEMAP_BLOCK_NUM, buf);
+
+    for(i = 0; i < NUM_OF_INODE_PER_BLOCK * INODELIST_BLOCKS; i++){        //0~63
+        if(buf[i] == 0)
+            return i;
+    }
+
+    return -1;
 }
 
 
 int GetFreeBlockNum(void)
 {
+    int i;
+    char* buf = (char*)malloc(BLOCK_SIZE);
 
+    DevReadBlock(BLOCK_BYTEMAP_BLOCK_NUM, buf);
+
+    for(i = 0; i < BLOCK_SIZE; i++){
+        if(buf[i] == 0)
+            return i;
+    }
+    
+    return -1;
 }
--- os_project/hw3/main.c
+++ os_project/hw3/main.c
@@ -1,8 +1,44 @@
 #include "fs.h"
 #include "disk.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
-	printf("Hello OS World\n");
+	int blknum, inodenum;
+	Inode inode, *pInode;
+	int *ptr;
+	FileSysInit();
+	DevCreateDisk();
+
+	inodenum = GetFreeInodeNum();
+	printf("1. %d\n", inodenum);
+
+	SetInodeBytemap(inodenum);
+	printf("inode byte map set %d\n", inodenum);
+
+	blknum = GetFreeBlockNum();
+	printf("3. %d\n", blknum);
+
+	SetBlockBytemap(blknum);
+	blknum = GetFreeBlockNum();
+	printf("4. %d\n", blknum);
+
+	inode.allocBlocks = 1;
+	inode.size = 2;
+	inode.type = 3;
+	inode.dirBlockPtr[0] = 0;
+	inode.dirBlockPtr[1] = 1;
+	inode.dirBlockPtr[2] = 1;
+	inode.dirBlockPtr[3] = 1;
+	inode.dirBlockPtr[4] = 1;
+
+	PutInode(inodenum, &inode);
+	pInode = (Inode*)malloc(sizeof(Inode));
+	printf("%d %d %d \n", pInode->allocBlocks, pInode->size, pInode->type);
+	GetInode(inodenum, pInode);
+
+	printf("%d %d %d \n", pInode->allocBlocks, pInode->size, pInode->type);
 	return 0;
 }
+ 
